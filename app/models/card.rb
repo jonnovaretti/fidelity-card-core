c6017038ff05be63c85f98c757d63842340e1f64
Jonathan Novaretti <jonn.novaretti@gmail.com>
2023-07-08 06:58:08 -0300

    Implementing update_score

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,10 +2,17 @@ class Card < ApplicationRecord
   belongs_to :customer
   belongs_to :company
 
-  enum status: %w[active completed cancelled expired]
+  enum :status, %w[active completed cancelled expired], default: :active
 
   validates :max_score, presence: true
 
   validates_numericality_of :score, greater_than_or_equal_to: 0, only_integer: true
   validates_numericality_of :max_score, greater_than: ->(card) { card.score }, only_integer: true
+
+  def update_score(new_score)
+    return unless active?
+
+    status = new_score == max_score ? :completed : :active
+    update(score: new_score, status: status)
+  end
 end
