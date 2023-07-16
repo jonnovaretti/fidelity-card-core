d59210ea86faa4d6fffabe10ebda2167007905ad
Jonathan Novaretti <jonn.novaretti@gmail.com>
2023-07-16 13:31:15 -0300

    Implementing menu profile suspended

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,13 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
+import { useClickOutside } from "stimulus-use"
 
 export default class extends Controller {
   static targets = [ "menuItems", "menu", "menuIcon" ]
   collapsed = false;
 
-  toggle() {
+  toggleSideMenu() {
     this.collapsed = !this.collapsed;
-    this.menuItemsTarget.className = this.collapsed ? "text-gray-400 transition duration-300" : "text-gray-400 hidden transition duration-300";
+    this.menuItemsTarget.className = this.collapsed ? "text-gray-400 transition duration-300" : "hidden";
     this.menuTarget.className = this.collapsed ? '' : 'h-15';
     this.menuIconTarget.className.baseVal = this.collapsed ? 'w-3 h-3 transition duration-300 rotate-90' : 'w-3 h-3 transition duration-300 rotate-180';
+  }
+
+  toggleTopMenu() {
+    useClickOutside(this);
+
+    this.collapsed = !this.collapsed;
+    this.menuItemsTarget.className = this.collapsed ? "absolute right-10 w-40 top-11 border border-gray-300 z-20" : "hidden";
+    this.menuIconTarget.className.baseVal = this.collapsed ? 'w-3 h-3 transition duration-300 rotate-90' : 'w-3 h-3 transition duration-300 rotate-180';
+  }
+
+  clickOutside() {
+    this.toggleTopMenu();
   }
 }
